e0c5b8c7bd00d37ac5605cd08a72ae1a315f8ec1
jeevan8184 <jeevanjatavath7@gmail.com>
2024-02-28 21:46:21 +0530

    fix typo

--- utils/database.js
+++ utils/database.js
@@ -8,7 +8,7 @@ export const connectToDB=async()=> {
     if(isConnected) return console.log('mongodb is already connected');
 
     try {
-        await mongoose.connect(process.env.MONGODB_URL,{
+        await mongoose.connect(process.env.MONGODB_URL.toString(),{
             dbName:'share_prompt',
             useNewUrlParser:true,
             useUnifiedTopology:true,
