b3aae1e110c39f8c8412b7ab0f7b6fe4dba2f62d
jeevan8184 <jeevanjatavath7@gmail.com>
2024-02-29 19:12:37 +0530

    fix typo

--- utils/database.js
+++ utils/database.js
@@ -8,8 +8,7 @@ export const connectToDB=async()=> {
     if(isConnected) return console.log('mongodb is already connected');
 
     try {
-        console.log('connection',"mongodb+srv://Prompt:Prompt@atlascluster.p0rj0jx.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster")
-        await mongoose.connect(process.env.MONGODB_URL,{
+        await mongoose.connect("mongodb+srv://Prompt:Prompt@atlascluster.p0rj0jx.mongodb.net/?retryWrites=true&w=majority&appName=AtlasCluster",{
             dbName:'share_prompt',
             useNewUrlParser:true,
             useUnifiedTopology:true,
